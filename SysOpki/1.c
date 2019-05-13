10ed379bf286c38b3a27de29d113fac5f2d24c2a
Simon1PL <saicxs123@gmail.com>
2019-05-13 21:58:36 +0200

    Update 1.c

--- SysOpki/1.c
+++ SysOpki/1.c
@@ -17,7 +17,7 @@
 int main(){
     #ifdef DLL
     void *handle = dlopen("./liblibrary.so", RTLD_LAZY);
-    void (*try_library)(char*)= dlsym(handle,"try_library");
+    void (*try_library)(char*)= dllimport(handle,"try_library");
     int size=dlsym(handle, "size");
     #endif
     
