4cafb4af0139575f1b1fe4591a73dc8d4336c659
Simon1PL <saicxs123@gmail.com>
2019-05-14 04:22:02 +0200

    Update main.c

--- Zadania/1/zad2/main.c
+++ Zadania/1/zad2/main.c
@@ -86,7 +86,7 @@ int parse_tasks(char** tasks, int i) {
 void print_results(){
 	printf("results:\n");
 	int i;
-	for (i=0; i<5000; i++){
+	for (i=0; i<49000; i++){
 		if (wyniki[i]!=NULL) printf("%s\n", wyniki[i]);
 	}
 }
