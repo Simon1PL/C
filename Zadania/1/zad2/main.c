b2cd3bdcd43b97f75df54e3454b60b57ed4641aa
Simon1PL <saicxs123@gmail.com>
2019-05-14 04:15:18 +0200

    Update main.c

--- Zadania/1/zad2/main.c
+++ Zadania/1/zad2/main.c
@@ -28,7 +28,11 @@ struct Times* my_clock=NULL;
 
 int main(int argc, char ** tasks) {
 	if (!tasks[1]) { help(); return-1; }
-	if (!strcmp(tasks[1], "report")) { make_raport(); return 1;}
+	if (!strcmp(tasks[1], "report")) {
+		make_raport(); 
+		print_results()
+		return 0;
+	}
 	if (!atoi(tasks[1])) {
 		printf ("niepoprawny rozmiar\n");
 		help();
