ad7099b096b657c41b8a8ff62b515be419b65250
Simon1PL <saicxs123@gmail.com>
2019-05-14 05:02:29 +0200

    Update main.c

--- Zadania/1/zad2/main.c
+++ Zadania/1/zad2/main.c
@@ -84,11 +84,11 @@ int parse_tasks(char** tasks, int i) {
 
 void print_results(){
 	printf("results:\n");
-	//int i;
-	//for (i=0; i<rozmiar; i++){
-		//printf("%d\n", i);
-//		if (wyniki[i]!=NULL) printf("%d\n", i);
-//	}
+	int i;
+	for (i=0; i<rozmiar; i++){
+		printf("%d\n", i);
+		if (wyniki[i]!=NULL) printf("%d\n", i);
+	}
 }
 
 void make_raport() {
