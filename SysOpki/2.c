6b6f976f2a3801bb8e94e088e25f708937bda9ce
Simon1PL <saicxs123@gmail.com>
2019-05-20 21:33:18 +0200

    Create 2.c

---
+++ SysOpki/2.c
@@ -0,0 +1,13 @@
+#include <fcntl.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/stat.h>
+#include <stdio.h> //printf
+#include<stdlib.h> //alloc
+
+
+int main(){
+    creat("./plik.txt", )
+    open("./plik.txt", )
+    return 0;
+}
