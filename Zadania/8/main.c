203507c7e8c51ee7c3f28d937c683a3be5c82b06
Simon1PL <saicxs123@gmail.com>
2019-05-21 03:22:07 +0200

    Update main.c

--- Zadania/8/main.c
+++ Zadania/8/main.c
@@ -121,7 +121,7 @@ int *blockFilter(void *threadNumber) {
     }
     gettimeofday(&end,NULL);
     int timeDifference = ((end.tv_sec - start.tv_sec) * 1000000) + (end.tv_usec - start.tv_usec);
-    return timeDifference;
+    return &timeDifference;
 }
 
 int *InterleavedFilter(void *threadNumber) {
@@ -136,15 +136,15 @@ int *InterleavedFilter(void *threadNumber) {
     }
     gettimeofday(&end,NULL);
     int timeDifference = ((end.tv_sec - start.tv_sec) * 1000000) + (end.tv_usec - start.tv_usec);
-    return timeDifference;
+    return &timeDifference;
 }
 
 void makeFilter(char *mode, int i, pthread_t *threads) {
    if(!strcmp(mode, "block")) {
-        pthread_create(&(threads[i]), NULL, blockFilter, &i);
+        pthread_create(&(threads[i]), NULL, (void *)blockFilter, &i);
     } 
     else {
-        pthread_create(&(threads[i]), NULL, InterleavedFilter, &i);
+        pthread_create(&(threads[i]), NULL, (void *)InterleavedFilter, &i);
     } 
 }
 
