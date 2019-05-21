d8c1d75bc51bf75e512a59f6c592339f1f9591fe
Simon1PL <saicxs123@gmail.com>
2019-05-21 04:12:45 +0200

    Update main.c

--- Zadania/8/main.c
+++ Zadania/8/main.c
@@ -59,7 +59,7 @@ void readImage(FILE *imageToRead) {
         int j;
         for(j = 0; j < image->height; j++) {
             fscanf(imageToRead, "%s", &value);
-             printf("\nvalue:%s\ni:%dj:%d", "a", i, j);
+             printf("\nvalue:%s\ni:%dj:%d", "a", "i", "j");
             image->data[i][j] = (char)atoi(value);
              printf("\naaa\n");
         }
