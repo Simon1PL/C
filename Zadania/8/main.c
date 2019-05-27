d26db22f63247f5d9bb9595585e184d49f1f79e5
Simon1PL <saicxs123@gmail.com>
2019-05-28 00:04:54 +0200

    Update main.c

--- Zadania/8/main.c
+++ Zadania/8/main.c
@@ -106,7 +106,7 @@ void filterMachine(Image *old, Filter *filter, int x, int y) {
             sum = image->data[wsp1][wsp2]*filter->data[i][j];
         }
     }
-    printf("%d %d %d\n",wsp1, wsp2, image->data[wsp1][wsp2]);
+    printf("%d %d %d\n",wsp1, wsp2, filter->data[i][j]);
     filteredImage->data[x][y] = (unsigned char) round(sum);
 }   
 
