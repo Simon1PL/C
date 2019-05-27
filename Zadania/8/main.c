b6603fc72b3fd485b2d021aa2a6e7001eef5532d
Simon1PL <saicxs123@gmail.com>
2019-05-28 00:16:02 +0200

    Update main.c

--- Zadania/8/main.c
+++ Zadania/8/main.c
@@ -80,6 +80,7 @@ void readFilter(FILE *filterToRead) {
         for(j = 0; j < filter->size; j++) {
             fscanf(filterToRead, "%f", value);
             filter->data[i][j] = strtof(value, NULL);
+            printf("%f \n", filter->data[i][j]);
         }
     }
 }
@@ -104,10 +105,9 @@ void filterMachine(Image *old, Filter *filter, int x, int y) {
             wsp1 = fmin(fmax(0, x - ceil(filter->size/2) + i + 1), image->width-1);
             wsp2 = fmin(fmax(0, y - ceil(filter->size/2) + j + 1), image->height-1);
             sum = image->data[wsp1][wsp2]*filter->data[i][j];
-            //printf(filter->data[i][j]);
         }
     }
-    printf("%d %d %d %lf %lf\n",wsp1, wsp2, image->data[wsp1][wsp2], filter->data[i-1][j-1], sum);
+    //printf("%d %d %d %lf %lf\n",wsp1, wsp2, image->data[wsp1][wsp2], filter->data[i-1][j-1], sum);
     filteredImage->data[x][y] = (unsigned char) round(sum);
 }   
 
