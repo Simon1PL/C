73c4bf29ac82b5759e9fb0ece98d2f6f250f5963
Simon1PL <saicxs123@gmail.com>
2019-06-07 22:48:58 +0200

    Update client.c

--- Zadania/10/client.c
+++ Zadania/10/client.c
@@ -6,21 +6,21 @@
 
 int main() {
     //create a socket
-    int socket = socket(AF_INET, SOCK_STREAM, 0);
+    int network_socket = socket(AF_INET, SOCK_STREAM, 0);
     //specify an address for the socket
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(9002); //0?
     server_address.sin_addr.s_addr = INADDR_ANY;
     //connect
-    int connection_status=connect(socket, (struct sockaddr *) &server_address, sizeof(server_address));
+    int connection_status=connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
     if (!connection_status) printf("connection ok\n");
     //recive data from the server
     char server_response[256];
-    recv(socket, &server_response, sizeof(server_response), 0);
+    recv(network_socket, &server_response, sizeof(server_response), 0);
     //print out the server's response
     printf("The server sent the data: %s\n", server_response);
     //close the socket
-    close(sock);
+    close(network_socket);
     return 0;
 }
