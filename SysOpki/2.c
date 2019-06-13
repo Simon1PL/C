0f27d97669efbf203e09563bfb4b9c3c5252e7a3
Simon1PL <saicxs123@gmail.com>
2019-06-13 20:22:57 +0200

    Update 2.c

--- SysOpki/2.c
+++ SysOpki/2.c
@@ -7,13 +7,10 @@
 
 
 int main(){
-    int fd;
-    fd = open("a.txt", O_WRONLY | O_APPEND | O_CREAT, 0644);
     //creat("plik.txt", O_RDWR);
-    int plik;
-    plik=open("plik.txt", O_RDWR | O_APPEND | O_CREAT, 0644);
+    int plik=open("plik.txt", O_RDWR | O_APPEND | O_CREAT);
     int ret=write(plik, "ala ma kota\n", 8);
-    printf("%d\n%d\n", plik, ret);
+
     close(plik);
     return 0;
 }
