f97af8a4e0a35a64580ca8b6d89decfbd6b1ae10
Simon1PL <saicxs123@gmail.com>
2019-06-13 19:45:27 +0200

    Update 2.c

--- SysOpki/2.c
+++ SysOpki/2.c
@@ -8,6 +8,15 @@
 
 int main(){
     //creat("./plik.txt", O_RDWR);
+    int filedesc = open("testfile.txt", O_WRONLY | O_APPEND);
+    if(filedesc < 0)
+        return 1;
+ 
+    if(write(filedesc,"This will be output to testfile.txt\n", 36) != 36)
+    {
+        write(2,"There was an error writing to testfile.txt\n");    // strictly not an error, it is allowable for fewer characters than requested to be written.
+        return 1;
+    }
     int plik=open("plik.txt", O_CREAT || O_RDWR);
     int ret=write(plik, "ala ma kota\n", 8);
     printf("%d\n", plik);
