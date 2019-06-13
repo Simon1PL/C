fd338c9db0c2e11eec40a3d5f5ef392f732e367c
Simon1PL <saicxs123@gmail.com>
2019-06-13 23:56:52 +0200

    Update 2.2.c

--- SysOpki/2/2.2.c
+++ SysOpki/2/2.2.c
@@ -61,7 +61,7 @@ int main(){
     chdir ("/home/students/s/z/szielins");
     nftw(".", fnFiles, 100, 0);
     printf("Files number: %d\n", files);
-    nftw(".", fnDires, 100, FTW_PHYS);
+    nftw(".", fnDires, 100, 0);
     printf("Dires number: %d\n", dires);
     return 0;
 }
