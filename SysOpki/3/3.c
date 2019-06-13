9f31b058a001931b726f7eac5f56084b20c1a292
Simon1PL <saicxs123@gmail.com>
2019-06-14 01:54:07 +0200

    Update 3.c

--- SysOpki/3/3.c
+++ SysOpki/3/3.c
@@ -3,6 +3,7 @@
 #include <stdlib.h> //alloc
 #include <sys/resource.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 int main(){
     pid_t myPid=getpid();
@@ -11,12 +12,16 @@ int main(){
     pid_t childPid=fork();
     if(childPid==0){
         //child (proces potomny)
-        system("ls -l");
-        execl("/bin/ls", "ls", "-l", NULL);
+        execl("/bin/ls", "ls", "-l", NULL); //odapala inny plik np. main2.c
         _exit(2);
     }
     else{
         //parent
+        wait(NULL); //wait(&status) to samo waitpid(-1, NULL, WNOHANG)
+        //WNOHANG-nie czeka tylko idzie dalej, trzeba jeszcze raz wywolac wait()
+        char status[500];
+        waitpid(childPid, &status, 0);
+        printf("WYNIK: %s\n", status);
     }
     return 0;
 }
