fd654e94d5e85a6a606fb76d75dda0efcac64182
Simon1PL <saicxs123@gmail.com>
2019-06-13 20:34:29 +0200

    Update 2.c

--- SysOpki/2/2.c
+++ SysOpki/2/2.c
@@ -8,9 +8,11 @@
 
 int main(){
     //creat("plik.txt", O_RDWR);
-    int plik=open("plik.txt", O_RDWR | O_APPEND | O_CREAT);
-    int ret=write(plik, "ala ma kota\na", 13);
-
+    int plik=open("plik.txt", O_RDWR | O_APPEND | O_CREAT | O_TRUNC);
+    write(plik, "Ala ma kota\n", 12);
+    char dane[12];
+    if(read(plik, dane, 12)!=12)
+        printf("blad odczytu %s", dane);
     close(plik);
     return 0;
 }
