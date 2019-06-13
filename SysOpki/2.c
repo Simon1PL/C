e895d0ac8d46ea1fcbb6572d8ebba318090a39bb
Simon1PL <saicxs123@gmail.com>
2019-06-13 19:47:02 +0200

    Update 2.c

--- SysOpki/2.c
+++ SysOpki/2.c
@@ -14,7 +14,7 @@ int main(){
  
     if(write(filedesc,"This will be output to testfile.txt\n", 36) != 36)
     {
-        write(2,"There was an error writing to testfile.txt\n");    // strictly not an error, it is allowable for fewer characters than requested to be written.
+        write(2,"There was an error writing to testfile.txt\n", 43);    // strictly not an error, it is allowable for fewer characters than requested to be written.
         return 1;
     }
     int plik=open("plik.txt", O_CREAT || O_RDWR);
