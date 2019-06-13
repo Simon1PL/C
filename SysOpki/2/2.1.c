0282a4296a04a658c5128fae0d0292b201fc4717
Simon1PL <saicxs123@gmail.com>
2019-06-13 22:46:52 +0200

    Update 2.1.c

--- SysOpki/2/2.1.c
+++ SysOpki/2/2.1.c
@@ -9,7 +9,7 @@
 int main(){
     //creat("plik.txt", O_RDWR);
     int plik=open("plik.txt", O_RDWR | O_APPEND | O_CREAT | O_TRUNC);
-    write(plik, "Ala ma kota\n", 12);ss
+    write(plik, "Ala ma kota\n", 12);
     char dane[11];
     lseek(plik, 0, SEEK_SET);
     if(read(plik, &dane, 11)!=11)
