e75467e1a4743ebbf5893999b272fcba3aeb9739
Simon1PL <saicxs123@gmail.com>
2019-06-14 03:48:06 +0200

    Update 3.c

--- SysOpki/4/3.c
+++ SysOpki/4/3.c
@@ -8,20 +8,23 @@
 
 void obsluga(int signal){
     printf("BOOM!\n");
-    exit(1);
+}
+
+void obsluga1(int signal, siginfo_t *dane, void *idk){
+    printf("DANE: %d, %s\n", dane.si_int, dane.si_ptr);
 }
 
 int main(){
     //signal(SIGINT, obsluga);
     struct sigaction act;
-    act.sa_handler=obsluga;
+    act.sa_handler=obsluga1;
     sigemptyset(&act.sa_mask);
     act.sa_flags=0;
     sigaction(SIGINT, &act, NULL);
     union sigval sig;
     sig.sival_int=1;
     sig.sival_ptr="ala ma kota";
-    //sigqueue(getpid(), SIGINT, sig); 
-    pause();
+    sigqueue(getpid(), SIGINT, sig); 
+    //pause(); //czeka na sygnal
     return 0;
 }
