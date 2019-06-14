ac03c3f60306ef0b5186069ac964752bd180313f
Simon1PL <saicxs123@gmail.com>
2019-06-14 04:31:38 +0200

    Update 3.c

--- SysOpki/5/3.c
+++ SysOpki/5/3.c
@@ -10,13 +10,13 @@ int main(){
         close(fd[1]);
         char dane[11];
         sleep(1);
-        if(read(fd[1], dane, 11)!=11)
+        if(read(fd[0], dane, 11)!=11)
             printf("abababa");
         printf("%s\n", dane);
     }
     else{
         close(fd[0]);
-        write(fd[0], "ala ma kota\n", 12);
+        write(fd[1], "ala ma kota\n", 12);
     }
     return 0;
 }
